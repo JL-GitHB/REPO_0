7aa0735b366b335cda98c1d614aaf7400f64191b
julian <julian.lari@sqs.com>
2020-11-11 10:50:47 +0100

    Add.

--- my-archetype-id/src/main/java/it/controllers/ControllerA.java
+++ my-archetype-id/src/main/java/it/controllers/ControllerA.java
@@ -7,4 +7,20 @@ public class ControllerA {
 		// TODO Auto-generated constructor stub
 	}
 
+	private String nome;
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
