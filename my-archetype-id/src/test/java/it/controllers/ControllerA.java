d3acf79e917f0449e9d02f41c23b312165cad858
julian <julian.lari@sqs.com>
2020-11-11 10:51:59 +0100

    Add tusk.

--- my-archetype-id/src/test/java/it/controllers/ControllerA.java
+++ my-archetype-id/src/test/java/it/controllers/ControllerA.java
@@ -2,4 +2,25 @@ package it.controllers;
 
 public class ControllerA {
 
+	public ControllerA() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private String nome;
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
