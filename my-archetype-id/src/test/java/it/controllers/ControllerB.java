2accfd877b03a623e4315513d1570f935cfeb65e
julian <julian.lari@sqs.com>
2020-11-11 11:11:54 +0100

    B

---
+++ my-archetype-id/src/test/java/it/controllers/ControllerB.java
@@ -0,0 +1,57 @@
+package it.controllers;
+
+public class ControllerB {
+
+	public ControllerB() {
+		super();
+	}
+
+	private String nome;
+
+	private String congome;
+
+	private String descrizione;
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the congome
+	 */
+	public String getCongome() {
+		return congome;
+	}
+
+	/**
+	 * @param congome the congome to set
+	 */
+	public void setCongome(String congome) {
+		this.congome = congome;
+	}
+
+	/**
+	 * @return the descrizione
+	 */
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	/**
+	 * @param descrizione the descrizione to set
+	 */
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+}
